de9083036a15b8f18ba08f842f1bfef74f43d5d2
Stéphanie Verissimo <stephanieverissimopp@gmail.com>
2022-11-13 20:06:59 -0300

    fix build error

---
+++ app/src/main/java/com/br/studygithubapi/presentation/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.br.studygithubapi.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.br.studygithubapi.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
